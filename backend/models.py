e9bfc91e798684fa1f3154fe6a63972bf34a254a
fd221 <eptar123@mail.ru>
2026-02-05 19:00:08 +0500

    feat: Добалено поле password_hash

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,4 @@ class User(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash : Mapped[str] = mapped_column(String(100), nullable=False)
