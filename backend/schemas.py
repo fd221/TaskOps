94c4224e39d4f465ea4200afe3e2dd91fe38bc65
fd221 <eptar123@mail.ru>
2026-02-05 19:04:23 +0500

    feat: Добавил схемы пользователей

---
+++ backend/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+# Схема для создания пользователя
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+# Схема для ответа
+class UserRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    email: EmailStr
