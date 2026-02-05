e4fc87df07d7ff0d954e8dee2e5b0058ccf2fb0e
fd221 <eptar123@mail.ru>
2026-02-05 19:08:27 +0500

    fix: исправлено название поля password_hash

--- backend/schemas.py
+++ backend/schemas.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, EmailStr, ConfigDict
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
-    password: str
+    password_hash: str
 
 
 # Схема для ответа
